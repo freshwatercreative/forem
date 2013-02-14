8af97b9e60c9c4cece2563aad9aa71dfece13630
James Martens <james.martens@freshwatercreative.com>
2013-02-14 14:22:40 -0700

    Fixing paths

--- app/assets/javascripts/forem/admin/members.js
+++ app/assets/javascripts/forem/admin/members.js
@@ -6,7 +6,7 @@
 (function($) {
   $(document).ready(function() {
     group_id = $('#add_member').data('group-id');
-    $('#new_member').autocomplete({source: '/admin/community/admin/users/autocomplete'})
+    $('#new_member').autocomplete({source: "/" + Forem.base_path + '/admin/users/autocomplete'})
 
     $("#new_member").bind("autocompleteselect", function(event, ui) {
       $("#add_member").attr('disabled', false)
@@ -14,7 +14,7 @@
 
     add_member = function() {
       user = $("#new_member").val()
-      $.post('/admin/community/admin/groups/' + group_id + '/members', { user: user })
+      $.post("/" + Forem.base_path + '/admin/groups/' + group_id + '/members', { user: user })
       $(this).attr('disabled', true)
       $("#new_member").val("")
       $('#members').append('<li>' + user + '</li>')
